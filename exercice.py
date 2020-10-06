8874da99133837c871a8e3f455c1c699206b7024
Rémy <remy.samkocwa@polymtl.ca>
2020-10-06 18:58:56 -0400

    Exercice 2

--- exercice.py
+++ exercice.py
@@ -6,6 +6,8 @@ import math, turtle
 
 
 # TODO: Définissez vos fonction ici
+
+# Exercice 1
 def ellipsoide(demi_axes, masse_volumique):
     x, *y, z = demi_axes
     volume = 4/3*math.pi()*x*y*z
@@ -13,6 +15,16 @@ def ellipsoide(demi_axes, masse_volumique):
     return (volume, masse)
 
 
+# Exercice 2
+def frequence(sentence: str) -> dict:
+    wordcount = {}
+    wordlist = list(sentence)
+    for elem in wordlist:
+        wordcount[elem] = wordlist.count(elem)
+    return sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
+def plus_haute_frequence(entree):
+    la_plus_haute = frequence(entree)
+    return la_plus_haute[0]
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
