848632d6d9cd30ae5859c66fe053de195a3da932
Rémy <remy.samkocwa@polymtl.ca>
2020-10-06 18:50:17 -0400

    Exercice 1
    Ellipsoide

--- exercice.py
+++ exercice.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math, turtle
 
 
 # TODO: Définissez vos fonction ici
+def ellipsoide(demi_axes, masse_volumique):
+    x, *y, z = demi_axes
+    volume = 4/3*math.pi()*x*y*z
+    masse = masse_volumique * volume
+    return (volume, masse)
+
 
 
 if __name__ == '__main__':
